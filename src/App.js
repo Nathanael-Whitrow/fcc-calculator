3469ed372367895c9ee64a4db21fa2ff6a059cf4
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-12 12:00:35 +1030

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Display() {
+  return (
+    <p>Display</p>
+  )
+}
+
+function Number() {
+  return (
+    <button>number button</button>
+  )
+}
+
+function ActionButton() {
+  return (
+    <button>Action button</button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <Display />
+        <Number />
+        <ActionButton />
+      </div>
     </div>
   );
 }
